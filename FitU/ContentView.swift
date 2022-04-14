8fc1715b273b544aa5e3aabe5fdeea5311a57592
trandrew24 <tranand@mail.gvsu.edu>
2022-04-14 17:30:17 -0400

    Initial Commit

---
+++ FitU/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitU
+//
+//  Created by Andrew Tran on 4/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitU/FitUApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitUApp.swift
+//  FitU
+//
+//  Created by Andrew Tran on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct FitUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
