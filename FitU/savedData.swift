b08fdfccca87ce8464c3a93db25f387f6c58e859
trandrew24 <tranand@mail.gvsu.edu>
2022-04-23 17:52:09 -0400

    one test realm only, no spacer for male yet

---
+++ FitU/savedData.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  FitU
+//
+//  Created by Andrew Tran on 4/23/22.
+//
+
+import Foundation
+
+class Storage: NSObject {
+    
+    static func archiveStringArray(object : [String]) -> Data {
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: object, requiringSecureCoding: false)
+            return data
+        } catch {
+            fatalError("Can't encode data: \(error)")
+        }
+
+    }
+
+    static func loadStringArray(data: Data) -> [String] {
+        do {
+            guard let array = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [String] else {
+                return []
+            }
+            return array
+        } catch {
+            fatalError("loadWStringArray - Can't encode data: \(error)")
+        }
+    }
+}
